9dfcb490418ded38b9614bcd287bb0c6244ae487
sr320 <roberts.sbr@gmail.com>
2020-11-10 10:31:48 -0800

    add comment for the if else code - what fish is big

--- scripts/fish-analysis.R
+++ scripts/fish-analysis.R
@@ -5,6 +5,9 @@ fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
 
 library(dplyr)
 
+
+# creating an addtional column that classifies fish bigger than 300mm as big (others as small)
+
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
 
